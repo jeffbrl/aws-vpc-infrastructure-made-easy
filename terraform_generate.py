e2ed30e23d2bffc58169fa756e480da4b9f4433b
Jeff Loughridge <jeffl@konekti.us>
2019-03-05 22:29:12 +0000

    Improve code readability

--- terraform_generate.py
+++ terraform_generate.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import datetime
+import logging
 from pathlib import Path
 import sys
 
@@ -12,19 +13,32 @@ from yaml.constructor import Constructor
 def add_bool(self, node):
     return self.construct_scalar(node)
 
-Constructor.add_constructor(u'tag:yaml.org,2002:bool', add_bool)
+
+Constructor.add_constructor("tag:yaml.org,2002:bool", add_bool)
 
 
 def file_exists(filename):
+    """ Returns true if file exists
+    :param filename: The file name to check
+    :type  filename: str
+    :returns: True if file exists
+    :rtype: bool
+    """
     file_handle = Path(filename)
     if file_handle.is_file():
         return True
     else:
         return False
 
-def parse_vpcs(input_yaml_file):
 
-    with open(input_yaml_file, 'r') as stream:
+def parse_vpcs(input_yaml_file):
+    """ Parses VPC configuration from YAML file
+    :param input_yaml_file: The YAML configuration file
+    :type input_yaml_file: str
+    :returns: dict representing the VPCs
+    :rtype: dict
+    """
+    with open(input_yaml_file, "r") as stream:
         try:
             vpcs = yaml.load(stream)
         except yaml.YAMLError as exc:
@@ -33,32 +47,57 @@ def parse_vpcs(input_yaml_file):
             sys.exit(1)
     return vpcs
 
+
 def write_tf_files(vpcs):
-    env = Environment(loader=FileSystemLoader('./'))
-    template = env.get_template('vpc.j2')
-    for vpc in vpcs['Vpcs']:
-      msg = template.render(vpc=vpc)
-      timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
-      output_file = f"terraform/{timestamp}-{vpc['name']}.tf"
-      try:
-          file_handle = Path(output_file)
-          file_handle.write_text(msg)
-      except Exception as e:
-          print(f"unable to write to {output_file}")
-          print(e)
-          sys.exit(1)
+    """ Creates terraform files using Jinja2 template
+    :param vpcs: dict representation of VPC configuration
+    :type vpcs: dict
+    """
+    env = Environment(loader=FileSystemLoader("./"))
+    template = env.get_template("vpc.j2")
+    for vpc in vpcs["Vpcs"]:
+        msg = template.render(vpc=vpc)
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
+        output_file = f"terraform/{timestamp}-{vpc['name']}.tf"
+        try:
+            file_handle = Path(output_file)
+            file_handle.write_text(msg)
+        except Exception as e:
+            print(f"unable to write to {output_file}")
+            print(e)
+            sys.exit(1)
 
-if __name__ == '__main__':
 
-    if not file_exists('vpc.j2'):
-        print('unable to open jinja2 template file vpcs.j2')
-        sys.exit(1)
-    if not file_exists('vpc_config.yml'):
-        print('unable to open YAML config file vpc_config.yml')
-        sys.exit(1)
+def validate_vpc_config(vpcs):
+    """ Validate VPC configuration and log errors to file
+    :param vpcs: dict representation of VPC configuration
+    :type vpcs: dict
+    :returns: True if VPC configuration is valid
+    :rtype: bool
+    """
+    # unimplemented
+    return True
 
-    vpcs = parse_vpcs(input_yaml_file='vpc_config.yml')
-    write_tf_files(vpcs)
 
-#    render_template(template, input_yaml_file='vpc_config.yml')
+def main():
 
+    if not file_exists("vpc.j2"):
+        print("unable to open jinja2 template file vpcs.j2")
+        return 1
+    if not file_exists("vpc_config.yml"):
+        print("unable to open YAML config file vpc_config.yml")
+        return 1
+
+    vpcs = parse_vpcs(input_yaml_file="vpc_config.yml")
+    if validate_vpc_config(vpcs):
+        write_tf_files(vpcs)
+    else:
+        print(
+            "Your YAML is valid but your VPC configuration is not. Check vpcs.log for troubleshooting"
+        )
+        return 1
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
